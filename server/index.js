13c312493ea152521297c5cab587bd0a8a424950
thekrprince <princekr700@gmail.com>
2026-05-04 23:30:18 +0530

    added server file to handle the code sent to backend

---
+++ server/index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const cors = require("cors");
+const Axios = require("axios");
+const app = express();
+const PORT = 9000;
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/compile", (req, res) => {
+    let code = req.body.code;
+    let language = req.body.language;
+    let input = req.body.input;
+
+    console.log("code:", code);
+    console.log("language:", language);
+    console.log("input:", input);
+
+    let languageMap = {
+        "javascript": { language: "javascript", version: "es15" },
+        "c": { language: "c", version: "10.2.0" },
+        "cpp": { language: "c++", version: "10.2.0" },
+        "python": { language: "python", version: "3.10.0" },
+        "java": { language: "java", version: "15.0.2" },
+    };
+
+    if (!languageMap[language]) {
+        return res.status(400).send({ error: "Unsupported language" });
+    }
+
+    let data = {
+        "language": languageMap[language].language,
+        "version": languageMap[language].version,
+        "files": [
+            {
+                "name": "main",
+                "content": code
+            }
+        ],
+        "stdin": input
+    };
+
+    let config = {
+        method: 'post',
+        url: 'https://emkc.org/api/v2/piston/execute',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+
+    Axios(config).then((response) => {
+        res.json(response.data.run);
+        console.log(response.data);
+    }).catch(err => {
+        console.log(error);
+        res.status(500).send({ error: "Something went wrong" });
+    });
+
+});
+
+app.listen(process.env.port || PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
